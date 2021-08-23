face7e74afc00b8640a4d61d0604e8e064460783
Christoph Beger <christoph.beger@medizin.uni-leipzig.de>
2021-08-23 15:25:37 +0200

    Update logo and favicon

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
     <q-header elevated class="bg-primary text-white">
       <q-toolbar>
         <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
+          <q-avatar square>
+            <img src="@/assets/logo.svg" />
           </q-avatar>
           Growth JS
         </q-toolbar-title>
