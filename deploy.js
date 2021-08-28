6331733e5774baf9e1a7bcb136a4a3797dcb98a9
Christoph Beger <ChristophB@users.noreply.github.com>
2021-08-28 21:56:00 +0200

    Add deployment script

---
+++ deploy.js
@@ -0,0 +1,25 @@
+var FtpDeploy = require("ftp-deploy");
+var ftpDeploy = new FtpDeploy();
+
+require('dotenv-flow').config();
+
+// FTP parameters like "FTP_USER" should be set in .env.local
+
+var config = {
+  user: process.env.FTP_USER,
+  password: process.env.FTP_PASSWORD,
+  host: process.env.FTP_HOST,
+  port: process.env.FTP_PORT,
+  localRoot: __dirname + "/dist/",
+  remoteRoot: process.env.FTP_REMOTE_ROOT,
+  include: ["*", "**/*"],
+  exclude: ["dist/**/*.map", "node_modules/**", "node_modules/**/.*", ".git/**"],
+  deleteRemote: true,
+  forcePasv: true,
+  sftp: true
+};
+
+ftpDeploy
+  .deploy(config)
+  .then(res => console.log("finished:", res))
+  .catch(err => console.log(err));
