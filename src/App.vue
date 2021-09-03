1d0ab973f50946533f944a3dfd31333f046f2487
Christoph Beger <ChristophB@users.noreply.github.com>
2021-09-03 18:48:56 +0200

    Make selected language fixed to 'Language'

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@
           emit-value
           map-options
           v-model="$root.$i18n.locale"
+          display-value="Language"
           :options="[
             { value: 'de', label: 'DE' },
             { value: 'en', label: 'EN' },
