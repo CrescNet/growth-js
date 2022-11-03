4064e0b948154da0033657128f554facc258ce76
Christoph Beger <christoph.beger@medizin.uni-leipzig.de>
2022-11-03 16:27:22 +0100

    Add references mixin

---
+++ src/mixins/references.ts
@@ -0,0 +1,65 @@
+import { ReferenceDataRow } from 'components/models'
+
+export default {
+  methods: {
+    sds_from_lms(value: number, l: number, m: number, s: number): number {
+      return (Math.abs(l) < 0.00001)
+        ? Math.log(value / m) / s
+        : ((value / m) ^ l - 1) / (l * s)
+    },
+
+    sds_from_reference(referenceData: ReferenceDataRow[], age: number, value: number): number | undefined {
+      const row = this.getMatchingReferenceRow(referenceData, age)
+      return !row ? undefined : this.sds_from_lms(value, row.l, row.m, row.s)
+    },
+
+    getMatchingReferenceRow(referenceData: ReferenceDataRow[], age: number): ReferenceDataRow | undefined {
+      let lower = undefined as ReferenceDataRow | undefined
+      let upper = undefined as ReferenceDataRow | undefined
+      const index = this.binsearchPos(referenceData.map(r => r.age), age)
+
+      if (index >= referenceData.length)
+        return referenceData[-1]
+      if (index === 0)
+        return referenceData[0]
+
+      if (referenceData[index].age === age) {
+        return referenceData[index]
+      }
+
+      if (referenceData[index].age > age) {
+        lower = referenceData[index - 1]
+        upper = referenceData[index]
+      }
+
+      if (lower && upper) {
+        return {
+          age: age,
+          l: this.approx(age, lower.age, lower.l, upper.age, upper.l),
+          m: this.approx(age, lower.age, lower.m, upper.age, upper.m),
+          s: this.approx(age, lower.age, lower.s, upper.age, upper.s)
+        } as ReferenceDataRow
+      }
+
+      return lower
+    },
+
+    approx(x: number, lowerX: number, lowerY: number, upperX: number, upperY: number): number {
+      return lowerY + (upperY - lowerY) * (x - lowerX) / (upperX - lowerX)
+    },
+
+    binsearchPos(array: number[], value: number) {
+      let low = 0
+      let high = array.length
+      while (low < high) {
+        let cur = Math.floor((high - low) / 2 + low)
+        if (value > array[cur]) {
+          low = cur + 1
+        } else {
+          high = cur
+        }
+      }
+      return low
+    }
+  }
+}
