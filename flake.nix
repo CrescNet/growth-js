f7d0a033aad421db242799910c76cb59e1ead2e2
Christoph Beger <christoph.beger@medizin.uni-leipzig.de>
2026-08-20 14:36:48 +0200

    build: Add nix flake dev environment

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Node.js development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_26
+            yarn
+            prettier
+            vue-language-server
+            typescript-language-server
+          ];
+        };
+      });
+}
