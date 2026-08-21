cb31210e9bfee70a686f525146aae7265b05d4f2
Christoph Beger <christoph.beger@medizin.uni-leipzig.de>
2026-08-21 15:47:46 +0200

    style: Reformat files

--- src/components/DateInput.svelte
+++ src/components/DateInput.svelte
@@ -5,10 +5,10 @@
 		value?: Date;
 	} = $props();
 
-	let internal = $derived(value?.toISOString().split("T", 1));
+	let internal = $derived(value?.toISOString().split('T', 1));
 
 	function handleInput(event: Event) {
-		const input = (event.currentTarget as HTMLInputElement).value
+		const input = (event.currentTarget as HTMLInputElement).value;
 		if (input !== undefined) {
 			const date = new Date(input);
 			if (date) {
