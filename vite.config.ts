b8d1639da792697de3d39cb7d5d4e03d99781c1c
Christoph Beger <christoph.beger@medizin.uni-leipzig.de>
2026-08-31 16:36:23 +0200

    build: Update deployment workflow
    build: Enable SPA mode for sveltekit
    
    build: Update workflow actions
    
    build: Set base path for production

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import tailwindcss from '@tailwindcss/vite';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
@@ -14,10 +14,15 @@ export default defineConfig({
 					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
 			},
 
-			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-			adapter: adapter()
+			adapter: adapter({
+				pages: 'build',
+				assets: 'build',
+				fallback: 'index.html',
+				strict: false
+			}),
+			paths: {
+				base: process.env.NODE_ENV === 'production' ? '/growth-js' : ''
+			}
 		}),
 
 		paraglideVitePlugin({
